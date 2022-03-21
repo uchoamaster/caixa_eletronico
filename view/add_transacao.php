d4662a67a11836a6fec8a2ea18798df31c4852ac
UchoaMaster <carlosjuchoa@hotmail.com>
2022-03-21 16:59:16 -0300

    new: alterando valores

--- view/add_transacao.php
+++ view/add_transacao.php
@@ -207,6 +207,5 @@ if(isset($_SESSION['login'])) {
                
                 </div>
             </div>
-
-            <script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
+			<script type="text/javascript" src="../assets/js/jquery-3.3.1.min.js"></script>
 			<script type="text/javascript" src="../assets/js/script.js"></script>
