1f158769bb26698ac3273c2eb0eee005c5b806bd
UchoaMaster <carlosjuchoa@hotmail.com>
2022-03-21 15:09:00 -0300

    new:classe de conexao configurada

---
+++ model/Conexao.class.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Conexao {
+
+    public static $instance;
+
+    public static function get_instance(){
+        if(!isset(self::$instance)){
+            self::$instance = new PDO("mysql:host=localhost; dbname=caixa_eletronico;port=8081", "root","",array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return self::$instance;
+    }
+}
+
+
+?>
